5d47cebc7f7d6f2f633f2462f5a1ddbeafce7209
sarea <sareakk@gmail.com>
2016-11-19 02:11:18 +0100

    we are connecting to mongo

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
 
 app.use(bodyParser.json());
+
+app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	next();
+})
 
 app.post('/api/message', function(req, res){
 	console.log(req.body);
 	res.status(200);
+});
+
+mongo.connect("mongodb://localhost:27017/test", function(err, db){
+	if(!err){
+		console.log("we are connected to mongodb");
+	}else{
+		console.log(err);
+	}
 });
 
 var server = app.listen(5000, function() {
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -1,11 +1,14 @@
 export class MainController {
-  constructor () {
+  constructor ($http) {
     'ngInject';
+
+    this.$http = $http;
 
   }
 
     postMessage() {
-      console.log("post");
+    	this.$http.post('http://localhost:5000/api/message', {msg: this.message});
+    	console.log("post");
     }
 
 }
