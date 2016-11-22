024b6937446fb716d760c1435c62ed39479a69a4
Zaher Alyousef <zaheralyousef@gmail.com>
2016-11-22 22:17:33 +0100

    root and started with authentication

---
+++ back-end/controllers/auth.js
@@ -0,0 +1,20 @@
+var User = require('../models/user');
+
+module.exports = {
+	register: function(req, res) {
+		User.findOne({ email: req.body.email }, function(err, existingUser) {
+			if (existingUser)
+				return res.status(409).send({message: 'Email is already registered'});
+			console.log(existingUser);
+			var user = new User(req.body);
+			user.save(function(err, result) {
+				if (err) {
+					res.status(500).send({
+						message: err.message
+					});
+				}
+				res.status(200);
+			})
+		});
+	}
+}
--- back-end/server.js
+++ back-end/server.js
@@ -1,46 +1,43 @@
+'use strict';
+
+//==================require======================
 var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //help the server to read json files.
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+var auth = require('./controllers/auth');
+var message = require('./controllers/message');
 
-var Message = mongoose.model('Message', {
-	msg: String
-})
-
+//==================initiating - middlewares=====
+var app = express();
 app.use(bodyParser.json());
-
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 	next();
-})
-
-app.post('/api/message', function(req, res){
-	console.log(req.body);
-	var message = new Message(req.body);
-	message.save();
-	res.status(200);
 });
 
-app.post('/auth/register', function(req, res){
-	console.log(req.body);
-})
 
-app.get('/api/message', GetMessages)
+//========================================
 
-function GetMessages(req, res){
-	Message.find({}).exec(function(err, result){
-		res.send(result);
-	});
-}
+app.post('/api/message', message.post);
 
-mongoose.connect("mongodb://localhost:27017/test", function(err, db){
-	if(!err){
+app.post('/auth/register', auth.register);
+
+app.get('/api/message', message.get);
+
+
+
+
+//======================================
+mongoose.connect("mongodb://localhost:27017/test", function(err, db) {
+	if (!err) {
 		console.log("we are connected to mongodb");
-	}else{
+	} else {
 		console.log(err);
 	}
 });
+
 
 var server = app.listen(5000, function() {
 	console.log("listening on port", server.address().port);
