3b6009b92653d90c5b6b4e70bd055cc7e0f86632
sarea <sareakk@gmail.com>
2016-12-01 09:27:35 +0100

    add gender, birthOfDate and city to db feilds in user schema, make new functionalty for dateOfbirth input

--- front-end/src/app/index.constants.js
+++ front-end/src/app/index.constants.js
@@ -13,7 +13,54 @@ export const DataModels = {
 		picURL: 'http://sim03.in.com/25/4060cd54ebb6cb5ac54cbda7eab8ec7c_m.jpg'
 	}],
 
-	months :['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+	months :[
+	{
+		value: 1,
+		name: 'January'
+	}, 
+	{
+		value: 2,
+		name: 'February'
+	}, 
+	{
+		value: 3,
+		name: 'March'
+	}, 
+	{
+		value: 4,
+		name: 'April'
+	}, 
+	{
+		value: 5,
+		name: 'May'
+	}, 
+	{
+		value: 6,
+		name: 'June'
+	}, 
+	{
+		value: 7,
+		name: 'July'
+	}, 
+	{
+		value: 8,
+		name: 'August'
+	}, 
+	{
+		value: 9,
+		name: 'September'
+	}, 
+	{
+		value: 10,
+		name: 'October'}, 
+	{
+		value: 11,
+		name: 'November'
+	}, 
+	{
+		value: 12,
+		name: 'December'
+	}],
 
 	month31 :[1, 2, 3, 4, 5, 6, 7 ,8 , 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
 
