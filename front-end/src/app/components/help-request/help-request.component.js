e9ea5bf423e947fb52652f7becf716a57e5a4ad2
sarea <sareakk@gmail.com>
2016-12-03 10:59:26 +0100

    edit user profile functionalty, some style and responsive, add Methods GET POST PUT DELETE OPTION to cors

--- front-end/src/app/components/help-request/help-request.component.js
+++ front-end/src/app/components/help-request/help-request.component.js
@@ -8,9 +8,9 @@ function HelpmeHelpRequestController($auth, $http, DataModels, $log) {
   'ngInject';
 
   var vm = this;
+  vm.$onInit = init;
+
   vm.$log = $log;
-  vm.$auth = $auth;
-  vm.$http = $http;
   vm.DataModels = DataModels;
   vm.postHelpRequest = postHelpRequest;
   vm.getHelpRequest = getHelpRequest;
@@ -25,59 +25,60 @@ function HelpmeHelpRequestController($auth, $http, DataModels, $log) {
   }
 
   function postHelpRequest() {
-
-    vm.token = vm.$auth.getToken()
+    var token = $auth.getToken()
     $http({
         method: 'POST',
         url: 'http://localhost:5000/api/helprequests',
         headers: {
-          'Authorization': vm.token
+          'Authorization': token
         },
         data: {
           'helprequest': vm.helprequest
         }
-    }).then(function successCallback(response) {
-              $log.log(response.data);
-            }, function errorCallback(response) {
-                  if (response.status = 401) { // If you have set 401
-                    $log.log("Some thing wrong!")
-                  }
-      });
+    })
+    .then(function(response) {
+        // success!
+        // redirect
+        // response = { data { question: { question_id: 5, title: '', question: ''}}}
+        //$state.go('questions.question',{questionId: response.data.question.question_id})
+        //x = response
+        console.log("post", response);
+    })
+    .catch(function(error, status) {
+     // display error
+        console.log(error);
+    });
   }
 
   function user() {
-    //   var vm = this;
-    //   var user_id = vm.$auth.getPayload();
-    //   console.log(vm.$auth);
-    //   this.$http.get('http://localhost:5000/api/user/').then(function (result) {
-    //   vm.sasa = result.data;
-    //   console.log(result);
-    // });
-    // var vm = this;
-    // vm.token = vm.$auth.getToken();      // user token
-    // vm.user_id = $auth.getPayload().sub; //user ID
-    // console.log(vm.user_id)
-    // this.$http({
-    //   method: "GET",
-    //   url: "http://localhost:5000/api/user/",
-    //   headers: {
-    //     Authorization: vm.token
-    //   }
-    // })
-    // .then(function(response) {
-    //     // success!
-    //     // redirect
-    //     // response = { data { question: { question_id: 5, title: '', question: ''}}}
+    var token = $auth.getToken();      // user token
+    var user_id = $auth.getPayload().sub; //user ID
+    console.log(user_id)
+    console.log(token)
+    $http({
+      method: "GET",
+      url: "http://localhost:5000/api/users/" + user_id,
+      // headers: {
+      //   'Authorization': vm.token
+      // }
+    })
+    .then(function(response) {
+        // success!
+        // redirect
+        // response = { data { question: { question_id: 5, title: '', question: ''}}}
 
-    //     //$state.go('questions.question',{questionId: response.data.question.question_id})
-    //     //x = response
-    //     console.log("post", response);
-    // })
-    // .catch(function(error, status) {
-    //  // display error
-    //     console.log(error);
-    // });
-    console.log("post");
+        //$state.go('questions.question',{questionId: response.data.question.question_id})
+        //x = response
+        console.log("post", response);
+    })
+    .catch(function(error, status) {
+     // display error
+        console.log(error);
+    });
+    //console.log("post");
+  }
+
+  function init() {
   }
 
 
--- front-end/src/app/components/profile/profile.component.js
+++ front-end/src/app/components/profile/profile.component.js
@@ -5,31 +5,24 @@ export var helpmeProfile = {
   templateUrl: 'app/components/profile/profile.html'
 };
 
-function HelpmeProfile($auth) {
+function HelpmeProfile($auth, $http) {
 	'ngInject';
 
 	var vm = this;
-	vm.$auth = $auth;
+    vm.$onInit = init;
+
     vm.isAuthenticated = $auth.isAuthenticated;
     vm.ratingAvg = ratingAvg;
+    vm.rating = 5;
+    vm.userInfo = {}
 
-    vm.userInfo = {
-    	firstName: 'charlize',
-    	lastName: 'theron',
-    	gender: 'Female',
-    	city: 'Amsterdam',
-    	dateOfBirth: new Date(1954, 2, 2),
+
+	function costomizeUserInfo(){
+        vm.userInfo.firstName = capitalizeFirstLetter(vm.userInfo.firstName);
+        vm.userInfo.lastName = capitalizeFirstLetter(vm.userInfo.lastName);
+        vm.userInfo.birth = new Date(vm.userInfo.dateOfbirth);
+        vm.userInfo.birth = vm.userInfo.birth.toLocaleDateString();
     }
-
-    vm.now = new Date().getFullYear();
-    //vm.birthDate = vm.userInfo.dateOfBirth.getUTCFullYear(); 			//this gives just the year
-
-	vm.dateOfBirth = vm.userInfo.dateOfBirth.toLocaleDateString();		//convert from UTC to string as dd/mm/yyy	
-	vm.firstName = capitalizeFirstLetter(vm.userInfo.firstName);
-	vm.lastName = capitalizeFirstLetter(vm.userInfo.firstName);
-	vm.rating = 2;
-
-	
 
     function ratingAvg(num){
     	return new Array(Number(num));
@@ -39,8 +32,60 @@ function HelpmeProfile($auth) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 
+    function user() {
+        var token = $auth.getToken();               // user token
+        var user_id = $auth.getPayload().sub;       // user ID
+        $http({
+          method: 'GET',
+          url: 'http://localhost:5000/api/users/' + user_id,
+        })
+        .then(function(response) {
+            // success!
+            // redirect
+            // response = { data { question: { question_id: 5, title: '', question: ''}}}
+            //$state.go('questions.question',{questionId: response.data.question.question_id})
+            //x = response
+            vm.userInfo = response.data;
+            costomizeUserInfo();
+        })
+        .catch(function(error, status) {
+         // display error
+            console.log(error);
+        });
+    }
 
+    function editUesr() {
+        vm.userInfo.lastName = 'theron';
+        var token = $auth.getToken();      // user token
+        var user_id = $auth.getPayload().sub; //user ID
+        $http({
+            method: 'PUT',
+            url: 'http://localhost:5000/api/users/' + user_id,
+            headers: {
+              'Authorization': token
+            },
+            data: {
+              'user': vm.userInfo
+            }
+        })
+        .then(function(response) {
+            // success!
+            // redirect
+            // response = { data { question: { question_id: 5, title: '', question: ''}}}
+            //$state.go('questions.question',{questionId: response.data.question.question_id})
+            //x = response
+            console.log("post", response);
+        })
+        .catch(function(error, status) {
+         // display error
+            console.log(error);
+        });
+    }
 
+    function init() {
+        user();
+        //editUesr();
+    }
 
 
 }
