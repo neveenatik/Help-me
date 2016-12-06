cdddab8470595b6d9eda6939f76bb0f8a4104943
AliAtrash <ali.atrash.nl@gmail.com>
2016-12-07 00:39:10 +0100

    Separated ‘profile’ from ‘help request’. Created a new status for operating profile from help request form. Added picture upload button to be added to profile - still under process.

---
+++ front-end/src/app/components/navbar/index.js
@@ -0,0 +1,8 @@
+import { helpmeProfile } from './profile.component';
+
+import { NavbarDirective } from './navbar.directive';
+
+export default angular.module('helpme.profile', [])
+  .component('helpmeProfile', helpmeProfile)
+  .name;
+  
--- front-end/src/app/components/navbar/navbar.directive.js
+++ front-end/src/app/components/navbar/navbar.directive.js
@@ -47,4 +47,14 @@ class NavbarController {
   logout() {
     this.$auth.logout();
   }
+
+  userProfileLoad() {
+    console.log("profile is clicked");
+    // go("./profile/")
+    // document.getElementById("user-profile-only").innerHTML='<object type="type/html" data="./profile/index.html" ></object>';
+    // this.$auth.location = './profile/index.html';
+    // $state.href("./profile/index.html")
+  }
 }
+
+
