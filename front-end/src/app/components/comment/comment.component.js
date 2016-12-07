9f23a8961e0fc4b34afed96b6c0cc503b6083e88
marwam7 <marwaxkillua@gmail.com>
2016-12-07 14:05:16 +0100

    add and delete comment component&&html

--- front-end/src/app/components/comment/comment.component.js
+++ front-end/src/app/components/comment/comment.component.js
@@ -9,25 +9,29 @@ function HelpmeCommentController($auth, $http, DataModels) {
   'ngInject';
 
   var vm = this;
-
+vm.$onInit = init;
   vm.DataModels = DataModels;
   vm.getComment = getComment;
   vm.postComment = postComment;
   vm.deleteComment = deleteComment;
   vm.editComment = editComment;
   vm.currentDate = Date.now();
-  //  vm.comments=[];
+    vm.comments={};
   vm.user = user;
 
-  function getComment() {
-    var vm = this;
-    vm.$http.get('http://localhost:5000/api/comments').then(function(result) {
-      //var user_id = vm.$auth.getPayload();
-      console.log(result);
-      vm.comments = result.data;
+function getComment() {
+    $http({
+      method: "GET",
+      url: "http://localhost:5000/api/comments",
+    })
+    .then(function(response) {
+        vm.comments = response.data;
+        console.log(vm.comments);
+    })
+    .catch(function(error, status) {
+        console.log(error);
     });
   }
-
   function postComment() {
     console.log('posted');
     var token = $auth.getToken()
@@ -43,6 +47,7 @@ function HelpmeCommentController($auth, $http, DataModels) {
       })
       .then(function(response) {
         console.log("post", response);
+        getComment();
       })
       .catch(function(error, status) {
         console.log(error);
@@ -50,6 +55,7 @@ function HelpmeCommentController($auth, $http, DataModels) {
   }
 
   function deleteComment(comment_id) {
+       var token = $auth.getToken()
     $http({
         method: 'DELETE',
         url: 'http://localhost:5000/api/comments/' + comment_id,
@@ -62,14 +68,15 @@ function HelpmeCommentController($auth, $http, DataModels) {
       })
       .then(function(response) {
         console.log(response.data);
-        return response.data;
+       vm.comments= response.data;
+        getComment();
       })
+    
   }
 
 
   function editComment(comment_id) {
-    var token = $auth.getToken(); // user token
-    //        var user_id = $auth.getPayload().sub; //user ID
+    var token = $auth.getToken()
     $http({
         method: 'PUT',
         url: 'http://localhost:5000/api/comments/' + comment_id,
@@ -81,7 +88,9 @@ function HelpmeCommentController($auth, $http, DataModels) {
         }
       })
       .then(function(response) {
-        console.log("post", response);
+        console.log("post", response.data);
+         vm.comments= response.data;
+         getComment();
       })
       .catch(function(error, status) {
         console.log(error);
@@ -105,6 +114,7 @@ function HelpmeCommentController($auth, $http, DataModels) {
 
   function init() {
     user();
+       getComment();
   }
 
 
